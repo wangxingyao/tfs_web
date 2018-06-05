dd3f60d0a1320a3b2edce221f936ac80f68eb79a
wangxingyao <wangxingyaochn@163.com>
2018-06-06 05:38:03 +0800

    tfs.py 修复tfs启动停止失败的bug, 并显示执行详细过程

--- tfs.py
+++ tfs.py
@@ -5,41 +5,41 @@ import sys, commands
 def usage():
     print "Usage: python tfs.py [status | start | stop]"
 
-def status():
-    cmds = [
-        "ssh NS 'ps aux | grep -v grep | grep /app/tfs/bin/nameserver'",
-        "ssh DS1 'ps aux | grep -v grep | grep /app/tfs/bin/dataserver'",
-        "ssh DS2 'ps aux | grep -v grep | grep /app/tfs/bin/dataserver'",
-        "ssh nginx 'ps aux | grep -v grep | grep /usr/local/nginx/sbin/nginx'"
-    ]
-    for cmd in cmds:
-        res = commands.getoutput(cmd)
+def execute(newcmds):
+    for host, cmd in newcmds:
+        print "[root@{} ~]# {}".format(host, cmd)
+        res = commands.getoutput("ssh {} '{}'".format(host, cmd))
         print res
-        if not res:
-            return False
-    return True
+
+def status():
+    newcmds = [
+        # ["host", "cmd"],
+        ["NS", "ps aux | grep -v grep | grep /app/tfs/bin/nameserver"],
+        ["DS1", "ps aux | grep -v grep | grep /app/tfs/bin/dataserver"],
+        ["DS2", "ps aux | grep -v grep | grep /app/tfs/bin/dataserver"],
+        ["nginx", "ps aux | grep -v grep | grep /usr/local/nginx/sbin/nginx"],
+    ]
+    execute(newcmds)
 
 def start():
-    cmds = [
-        "ssh DS1 '/app/tfs/scripts/tfs start_ds 1-2'",
-        "ssh DS2 '/app/tfs/scripts/tfs start_ds 1-2'",
-        "ssh NS '/app/tfs/scripts/tfs start_ns'",
-        "ssh nginx '/usr/local/nginx/sbin/nginx'"
+    newcmds = [
+        # ["host", "cmd"],
+        ["DS1", "/app/tfs/scripts/tfs start_ds 1-2"],
+        ["DS2", "/app/tfs/scripts/tfs start_ds 1-2"],
+        ["NS", "/app/tfs/scripts/tfs start_ns"],
+        ["nginx", "/usr/local/nginx/sbin/nginx"],
     ]
-    for cmd in cmds:
-        res = commands.getoutput(cmd)
-        print res
+    execute(newcmds)
 
 def stop():
-    cmds = [
-        "ssh nginx '/usr/local/nginx/sbin/nginx -s stop'"
-        "ssh NS '/app/tfs/scripts/tfs stop_ns'",
-        "ssh DS1 '/app/tfs/scripts/tfs stop_ds_all'",
-        "ssh DS2 '/app/tfs/scripts/tfs stop_ds_all'",
+    newcmds = [
+        # ["host", "cmd"],
+        ["nginx", "/usr/local/nginx/sbin/nginx -s stop"],
+        ["NS", "/app/tfs/scripts/tfs stop_ns"],
+        ["DS1", "/app/tfs/scripts/tfs stop_ds_all"],
+        ["DS2", "/app/tfs/scripts/tfs stop_ds_all"],
     ]
-    for cmd in cmds:
-        res = commands.getoutput(cmd)
-        print res
+    execute(newcmds)
 
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == "status":
